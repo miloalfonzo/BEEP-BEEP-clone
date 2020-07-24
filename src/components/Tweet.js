fea9e2a23965029cbc5333441861da377554e975
Milo! <milagrosalfonzo@outlook.com.ar>
2020-07-23 21:13:27 -0300

    changed style of the icons

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -12,6 +12,7 @@ function Tweet({ img, name, user, tweet }) {
       line-height: 10px;
     }
     i {
+      padding: 5px;
       color: #686f75;
       font-size: 20px;
     }
@@ -28,6 +29,21 @@ function Tweet({ img, name, user, tweet }) {
     // .fa-chevron-down {
     //   margin-left: 25vw;
     // }
+    .blue:hover {
+      background-color: rgba(29, 161, 242, 0.1);
+      border-radius: 50px;
+      color: #1e7ac3;
+    }
+    .green:hover {
+      background-color: rgb(11, 161, 151, 0.1);
+      border-radius: 50px;
+      color: rgb(23, 191, 99);
+    }
+    .red:hover {
+      background-color: rgba(224, 36, 94, 0.1);
+      border-radius: 50px;
+      color: rgb(224, 36, 94);
+    }
   `;
 
   return (
@@ -44,10 +60,10 @@ function Tweet({ img, name, user, tweet }) {
           {/* <i className=" mt-3 fas fa-chevron-down"></i> */}
         </div>
         <div className="row icons mb-3">
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="far fa-share-square"></i>
+          <i className="blue far fa-comment"></i>
+          <i className="green fas fa-retweet"></i>
+          <i className="red far fa-heart"></i>
+          <i className="blue far fa-share-square"></i>
         </div>
         <div className="row line"></div>
       </div>
